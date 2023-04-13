8989b0a6f5813c193191e6f047d38b6a9f085876
muing <ungetqqq@gmail.com>
2023-04-13 17:11:39 +0900

    Central indicator done.

--- lib/Back/Provider/Providers/AttitudeDataProvider.dart
+++ lib/Back/Provider/Providers/AttitudeDataProvider.dart
@@ -1,11 +1,20 @@
 // ignore_for_file: file_names
+import 'dart:collection';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class AttitudeDataProvider extends ChangeNotifier {
   /* States */
   double pitch = 0.0;
   double roll = 0.0;
   double yaw = 0.0;
+
+  static const int _scopeSize = 15;
+  final Queue<double> _pitchQueue = Queue<double>();
+  final Queue<double> _yawQueue = Queue<double>();
+
+  final AngleMovingAverage _rollFilter =
+      AngleMovingAverage(scopeSize: _scopeSize);
 
   /* Actions */
   void updatePitchRollYaw(String rawStr) {
@@ -14,10 +23,48 @@ class AttitudeDataProvider extends ChangeNotifier {
       return;
     }
 
-    pitch = double.parse(splitedRawString[0]);
-    roll = -(double.parse(splitedRawString[1]));
-    yaw = double.parse(splitedRawString[2]);
+    _pitchQueue.add(double.parse(splitedRawString[0]));
+    _yawQueue.add(double.parse(splitedRawString[2]));
+
+    if (_pitchQueue.length > _scopeSize) {
+      _pitchQueue.removeFirst();
+      _yawQueue.removeFirst();
+    }
+
+    pitch = _pitchQueue.reduce((a, b) => a + b) / _pitchQueue.length;
+    roll = _rollFilter.update(double.parse(splitedRawString[1]));
+    yaw = _yawQueue.reduce((a, b) => a + b) / _yawQueue.length;
 
     notifyListeners();
+  }
+}
+
+class AngleMovingAverage {
+  int scopeSize;
+  List<double> cosBuffer;
+  List<double> sinBuffer;
+  int index = 0;
+
+  AngleMovingAverage({required this.scopeSize})
+      : cosBuffer = List.generate(scopeSize, (i) => 0.0),
+        sinBuffer = List.generate(scopeSize, (i) => 0.0);
+
+  double update(double angle) {
+    double radian = angle * math.pi / 180;
+    double cosValue = math.cos(radian);
+    double sinValue = math.sin(radian);
+
+    cosBuffer[index] = cosValue;
+    sinBuffer[index] = sinValue;
+    index = (index + 1) % scopeSize;
+
+    double cosSum = cosBuffer.fold(0.0, (acc, val) => acc + val);
+    double sinSum = sinBuffer.fold(0.0, (acc, val) => acc + val);
+
+    double cosAvg = cosSum / scopeSize.toDouble();
+    double sinAvg = sinSum / scopeSize.toDouble();
+
+    double avgAngle = math.atan2(sinAvg, cosAvg) * 180 / math.pi;
+    return avgAngle;
   }
 }
--- lib/Front/Component/AttitudeGuidance/AttitudeGuidance.dart
+++ lib/Front/Component/AttitudeGuidance/AttitudeGuidance.dart
@@ -22,15 +22,66 @@ class AttitudeGuidanceState extends State<AttitudeGuidance> {
       aspectRatio: 1,
       child: Stack(
         children: [
+          // Fixed ref
           SvgPicture.asset('assets/image/attitudeguidance_fixedref.svg'),
+          // Core
+          Container(
+            alignment: Alignment.center,
+            // padding: const EdgeInsets.all(0),
+            child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+              const double widthRatio = 0.77;
+              const double heightRatio = 0.77;
+              return SizedBox(
+                  width: constraints.maxWidth * widthRatio,
+                  height: constraints.maxHeight * heightRatio,
+                  child: ClipOval(
+                    clipper: AttitudeGuidanceCoreClipper(),
+                    child: LayoutBuilder(builder:
+                        (BuildContext context, BoxConstraints constraints) {
+                      return Transform.rotate(
+                        angle: attitudeData.roll * math.pi / 180,
+                        child: Transform(
+                          alignment: Alignment.center,
+                          transform: Matrix4.identity()
+                            ..translate(
+                                0.0,
+                                constraints.maxHeight *
+                                    ((attitudeData.pitch / 90.0) * 1.943))
+                            ..scale(5.3),
+                          child: SvgPicture.asset(
+                              'assets/image/attitudeguidance_core.svg'),
+                        ),
+                      );
+                    }),
+                  ));
+            }),
+          ),
+          // Bank angle guide
           Transform.rotate(
             angle: attitudeData.roll * math.pi / 180,
             child:
                 SvgPicture.asset('assets/image/attitudeguidance_bankguide.svg'),
           ),
+          // Fixed ref
           SvgPicture.asset('assets/image/attitudeguidance_fixedref_core.svg'),
         ],
       ),
     );
+  }
+}
+
+class AttitudeGuidanceCoreClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    final double radius = math.min(size.width, size.height) * 0.5;
+
+    return Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2), radius: radius);
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return false;
   }
 }
